6d9a46dd14bc8f7e56d12bd56de0fe422d85c0f9
Madhav Varshney <madhavarshney@gmail.com>
2020-09-08 12:29:54 -0700

    Improve snowpack config?

--- frontend/snowpack.config.js
+++ frontend/snowpack.config.js
@@ -1,7 +1,24 @@
+// TODO: this is not an officially supported way of using the plugin
+// and may break with an update
+require('@snowpack/plugin-dotenv')()
+
+let API_URL;
+
+if (process.env.NODE_ENV == 'development') {
+  API_URL = process.env.SNOWPACK_PUBLIC_API_URL || 'http://localhost:5001/'
+  process.env.SNOWPACK_PUBLIC_API_URL = '/api'
+}
+
 module.exports = {
+  mount: {
+    'src': '/',
+    'static': '/'
+  },
   plugins: [
-    '@snowpack/plugin-dotenv',
     // Optimize production builds with Webpack
     ['@snowpack/plugin-webpack', {}]
-  ]
+  ],
+  proxy: {
+    '/api': API_URL,
+  }
 }
