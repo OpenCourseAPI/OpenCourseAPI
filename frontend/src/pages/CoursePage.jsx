39f4e46fe6b65f2ce30d1088b8d685ad8cf7f8b0
Madhav Varshney <madhavarshney@gmail.com>
2020-09-09 12:08:58 -0700

    Refactor classes table shown on dept, course pages

--- frontend/src/pages/CoursePage.jsx
+++ frontend/src/pages/CoursePage.jsx
@@ -1,27 +1,13 @@
 import { h, Fragment } from 'preact'
-import { useState, useEffect } from 'preact/hooks'
 
 import { campus, PATH_PREFIX } from '../data'
 import { useApi } from '../state'
+import { formatDate } from '../utils'
 import BreadCrumbs from '../components/BreadCrumbs'
-import { CampusNotFound, DeptNotFound, CourseNotFound } from '../components/NotFound'
+import ClassesTable from '../components/ClassTable'
+import { CampusNotFound, CourseNotFound } from '../components/NotFound'
 
-const opt = { year: 'numeric', month: 'short', day: 'numeric' }
-const formatDate = (str) => new Date(Date.parse(str)).toLocaleDateString('en-US', opt)
-const replaceTBA = (text) => text === 'TBA' ? <span class="none">(none)</span> : text
-
-const displayTimes = (time) => {
-  const time_string = time.start_time == 'TBA' ? replaceTBA('TBA') : `${time.start_time} - ${time.end_time}`
-  return (
-    <>
-      <td>{time.instructor}</td>
-      <td>{replaceTBA(time.days)}</td>
-      <td>{time_string}</td>
-      <td>{time.location}</td>
-    </>
-  )
-    // <td>${time.room}</td>
-}
+const dateFormatOpts = { year: 'numeric', month: 'short', day: 'numeric' }
 
 export default function CoursePage({ college, dept, course }) {
   const colleged = campus.find((cmp) => cmp.id === college)
@@ -29,47 +15,25 @@ export default function CoursePage({ college, dept, course }) {
   if (!colleged) return <CampusNotFound />
 
   const [classes, error] = useApi(`/${college}/depts/${dept}/courses/${course}/classes`)
-  const row_els = []
-  const first = (classes && classes[0]) || {}
-  const hasSeatInfo = classes && classes[0] ? (classes[0].status && classes[0].seats != undefined) : true
-  const headers = ['CRN', 'Start', 'End', ...(hasSeatInfo ? ['Status', 'Seats', 'Waitlist'] : []), 'Professor', 'Days', 'Time', 'Location']
-  // <th>Room</th>
 
-  if (classes) {
-    for (const section of classes) {
-      const start = formatDate(section.start)
-      const end = formatDate(section.end)
-
-      const rows = section.times.length
-      const table_rows = [
-        // section.CRN,
-        section.CRN.toString().padStart(5, '0'),
-        start,
-        end,
-        ...(hasSeatInfo ? [
-          section.status,
-          section.seats,
-          section.wait_cap ? `${section.wait_seats}/${section.wait_cap}` : section.wait_seats
-        ] : [])
-      ]
-
-      row_els.push(
-        <tr>
-          {table_rows.map((name) => <td rowspan={rows}>{name}</td>)}
-          {displayTimes(section.times[0])}
-        </tr>
-      )
-
-      for (const time of section.times.slice(1)) {
-        row_els.push(
-          <tr>
-            {displayTimes(time)}
-          </tr>
-        )
-      }
-    }
-  }
-
+  const first = classes && classes[0]
+  const hasSeatInfo = first ? (first.status && first.seats != undefined) : false
+  const headers = [
+    'CRN',
+    'Start',
+    'End',
+    ...(
+      hasSeatInfo ? [
+        'Status',
+        'Seats',
+        'Waitlist'
+      ] : []
+    ),
+    'Professor',
+    'Days',
+    'Time',
+    'Location',
+  ]
   const crumbs = [
     { url: '/', name: 'Home' },
     { url: `${PATH_PREFIX}/${college}`, name: colleged.name },
@@ -84,17 +48,28 @@ export default function CoursePage({ college, dept, course }) {
   } else {
     content = (
       <>
-        <p style={{ marginTop: 0 }}>{first.title}&nbsp; · &nbsp;{first.units} units</p>
-        <div class="table-container">
-          <table class="classes data">
-            <thead>
-              <tr>
-                {headers.map((name) =>  <th>{name}</th>)}
-              </tr>
-            </thead>
-            <tbody>{row_els}</tbody>
-          </table>
-        </div>
+        <p style={{ marginTop: 0 }}>
+          {(first && first.title) || ''}&nbsp; · &nbsp;{first ? first.units : 'X'} units
+        </p>
+        <ClassesTable
+          headers={headers}
+          classes={classes}
+          getClassColumns={(section) => {
+            const start = formatDate(section.start, dateFormatOpts)
+            const end = formatDate(section.end, dateFormatOpts)
+
+            return [
+              section.CRN.toString().padStart(5, '0'),
+              start,
+              end,
+              ...(hasSeatInfo ? [
+                section.status,
+                section.seats,
+                section.wait_cap ? `${section.wait_seats}/${section.wait_cap}` : section.wait_seats
+              ] : [])
+            ]
+          }}
+        />
       </>
     )
   }
